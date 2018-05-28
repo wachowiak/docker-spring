6620c8a62c076c106d775c8fd1e44cb07103f252
wachowiak.org <marcin.wachowiak@gmail.com>
2018-05-28 14:45:27 +0200

    spring with docker - init

---
+++ src/main/java/org/wachowiak/docker/DockerApplication.java
@@ -0,0 +1,12 @@
+package org.wachowiak.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
