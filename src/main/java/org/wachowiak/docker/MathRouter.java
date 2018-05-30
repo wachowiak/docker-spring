6661a88cead93b90bdffa99185373a3abba1d9d2
wachowiak <marcin.wachowiak@gmail.com>
2018-05-30 15:09:47 +0200

    switch from spring-web to webflux

---
+++ src/main/java/org/wachowiak/docker/MathRouter.java
@@ -0,0 +1,20 @@
+package org.wachowiak.docker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class MathRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(MathHandler mathHandler) {
+
+        return RouterFunctions
+                .route(RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), mathHandler::hello);
+    }
+}
